0b9e791de69554a62c07b4bb78754a32215db1a3
zJiaC <425226133@qq.com>
2024-04-21 14:13:53 +0000

    Deploying to master from @ zJiaC/tech-arch-doc-source-code@67d0fe207fa8c444cff18f35fb192f6ed3c9f53a 🚀

---
+++ assets/That's Why (You Go Away).html-CLyXeVZ4.js
@@ -0,0 +1,94 @@
+import{_ as i}from"./plugin-vue_export-helper-DlAUqK2U.js";import{r as s,o as a,c as l,a as d,b as e,d as t}from"./app-CUJPFFrd.js";const o={},r=e("h1",{id:"that-s-why-you-go-away",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#that-s-why-you-go-away"},[e("span",null,"That's Why (You Go Away)")])],-1),v=e("h2",{id:"翻唱",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#翻唱"},[e("span",null,"翻唱")])],-1),c=t(`<h2 id="歌词" tabindex="-1"><a class="header-anchor" href="#歌词"><span>歌词</span></a></h2><div class="language-text line-numbers-mode" data-ext="text" data-title="text"><pre class="language-text"><code>作词 : Jascha Richter
+
+作曲 : Jascha Richter
+
+Baby won&#39;t you tell me why there is sadness in your eyes
+宝贝，你真地不愿告诉我吗 为什么你的眼中含着伤悲？
+
+I don&#39;t wanna say goodbye to you
+我不想就此与你告别
+
+Love is one big illusion I should try to forget
+爱是场华丽错觉 我该尽力忘却
+
+but there is something left in my head
+但有些事还留在我的脑海里，难以挥别
+
+You&#39;re the one who set it up now you&#39;re the one to make it stop
+当初一切因你开始 现在一切因你终止
+
+I&#39;m the one who&#39;s feeling lost right now
+而此刻的我感到深深迷失
+
+Now you want me to forget every little thing you said
+现在你要我忘了
+
+but there is something left in my head
+你说过的每一件事
+
+Chorus:I won&#39;t forget the way you&#39;re kissing
+我不会忘记你吻我的方式
+
+The feeling&#39;s so strong were lasting for so long
+那些强烈的感情曾经延续了这么长的时间
+
+But I&#39;m not the man your heart is missing
+但我并不是那个你心中正在想念的人
+
+That&#39;s why you go away I know
+我知道，那就是为何你选择离别
+
+You were never satisfied no matter how I tried
+你总是无法满意 无论我如何努力
+
+Now you wanna say goodbye to me
+现在你要对我说出再见
+
+Love is one big illusion I should try to forget
+爱是场华丽错觉 我该尽力忘却
+
+but there is something left in my head
+但有些事还留在我的脑海里，难以挥别
+
+Chorus:I won&#39;t forget the way you&#39;re kissing
+我将永生难忘你是如何与我热吻
+
+The feeling&#39;s so strong were lasting for so long
+那些强烈的感情曾经延续了这么长的时间
+
+But I&#39;m not the man your heart is missing
+但我并不是那个你心中正在想念的人
+
+That&#39;s why you go away I know
+我知道，那就是为何你选择离别
+
+Sitting here all alone in the middle of nowhere
+孤独地坐在这 仿佛身处无垠荒野
+
+Don&#39;t know which way to go
+不知该何去何从
+
+There ain&#39;t so much to say now between us
+你我之间现在已没有太多话要说
+
+There ain&#39;t so much for you
+没有太多话要对你说
+
+There ain&#39;t so much for me anymore
+也不再有那么多话要对我说
+
+Chorus:I won&#39;t forget the way you&#39;re kissing
+我将永生难忘你是如何与我热吻
+
+The feeling&#39;s so strong were lasting for so long
+那些强烈的感情 曾经延续了这么长的时间
+
+But I&#39;m not the man your heart is missing
+但我并不是那个你心中正在想念的人
+
+That&#39;s why you go away I know
+我知道，那就是为何你选择离别
+
+That&#39;s why you go away I know
+我知道，那就是为何你选择离别
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,2);function u(m,h){const n=s("VidStack");return a(),l("div",null,[r,v,d(n,{src:"http://tx.stream.kg.qq.com/gzc-kgsvp/1021_s_0b53k3wpyaaluyah3fhcovs3ivwd7qoqx3ca.f0.m4a?vkey=59FEB2306747FE5C27EF6D7AB8E8FD6C836D7FCFE149A84283C1ECDC552C514813A1089F03D6D1FC9BCF9C07B6D4644FAA62DB080C450E4EB994573D9213996F106EC28CF721E9A3D872DE2B5FAFB439E2F350F0ECD79563&dis_k=d1ad7cebdddb9a858fe8c051afbdb0dd&dis_t=1713412490&fromtag=1021&ugcid=15142987_1678194080_177&nr=1",title:"That's Why (You Go Away)",poster:"/logo.jpg"}),c])}const g=i(o,[["render",u],["__file","That's Why (You Go Away).html.vue"]]),p=JSON.parse(`{"path":"/posts/english/That's%20Why%20(You%20Go%20Away).html","title":"That's Why (You Go Away)","lang":"zh-CN","frontmatter":{"title":"That's Why (You Go Away)","icon":"language","date":"2023-03-13T00:00:00.000Z","category":["English"],"description":"That's Why (You Go Away) 翻唱","head":[["meta",{"property":"og:url","content":"https://zjiac.github.io/tech-arch-doc/tech-arch-doc/posts/english/That's%20Why%20(You%20Go%20Away).html"}],["meta",{"property":"og:site_name","content":"zJiac 个人知识体系"}],["meta",{"property":"og:title","content":"That's Why (You Go Away)"}],["meta",{"property":"og:description","content":"That's Why (You Go Away) 翻唱"}],["meta",{"property":"og:type","content":"article"}],["meta",{"property":"og:locale","content":"zh-CN"}],["meta",{"property":"og:updated_time","content":"2024-04-20T02:08:45.000Z"}],["meta",{"property":"article:author","content":"zJiaC"}],["meta",{"property":"article:published_time","content":"2023-03-13T00:00:00.000Z"}],["meta",{"property":"article:modified_time","content":"2024-04-20T02:08:45.000Z"}],["script",{"type":"application/ld+json"},"{\\"@context\\":\\"https://schema.org\\",\\"@type\\":\\"Article\\",\\"headline\\":\\"That's Why (You Go Away)\\",\\"image\\":[\\"\\"],\\"datePublished\\":\\"2023-03-13T00:00:00.000Z\\",\\"dateModified\\":\\"2024-04-20T02:08:45.000Z\\",\\"author\\":[{\\"@type\\":\\"Person\\",\\"name\\":\\"zJiaC\\",\\"url\\":\\"https://zjiac.github.io/tech-arch-doc/\\"}]}"]]},"headers":[{"level":2,"title":"翻唱","slug":"翻唱","link":"#翻唱","children":[]},{"level":2,"title":"歌词","slug":"歌词","link":"#歌词","children":[]}],"git":{"createdTime":1678770832000,"updatedTime":1713578925000,"contributors":[{"name":"zJiaC","email":"425226133@qq.com","commits":6}]},"readingTime":{"minutes":2.49,"words":747},"filePathRelative":"posts/english/That's Why (You Go Away).md","localizedDate":"2023年3月13日","excerpt":"\\n<h2>翻唱</h2>\\n","autoDesc":true}`);export{g as comp,p as data};
